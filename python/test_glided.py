24bebc4677088cc9fb8279198f02ee887e4f4d01
Adrian Stobbe <stobbe.adrian@gmail.com>
2022-03-13 16:56:33 +0100

    refactor

--- python/test_glided.py
+++ python/test_glided.py
@@ -2,6 +2,22 @@ import pytest
 from gilded_rose import *
 
 
+def get_new_quality_with_name(name, old_quality, sellin):
+    items = [Item(name, sellin, old_quality)]
+    create_and_update(items)
+    return items[0].quality
+
+
+def get_new_sellin_with_name(name, quality, sellin):
+    items = [Item(name, sellin, quality)]
+    create_and_update(items)
+    return items[0].sellin
+
+
+def get_new_quality(old_quality, sellin):
+    return get_new_quality_with_name("item", old_quality, sellin)
+
+
 def create_and_update(items):
     sut = GildedRose(items)
     sut.update_quality()
@@ -9,40 +25,23 @@ def create_and_update(items):
 
 @pytest.mark.parametrize("quality", [0, 1, 2, 3, 50])
 def test_never_negative(quality):
-    items = [Item("item", 0, quality)]
-
-    create_and_update(items)
-    assert items[0].quality >= 0
+    assert get_new_quality(quality, 2) >= 0
 
 
 def test_lower_quality():
     quality = 1
     quality_2 = 2
-    items = [Item("item", 10, quality), Item("item", 10, quality_2)]
-
-    create_and_update(items)
-    assert items[0].quality == quality - 1
-    assert items[1].quality == quality_2 - 1
+    assert get_new_quality(quality, 10) == quality - 1
+    assert get_new_quality(quality_2, 10) == quality_2 - 1
 
 
-def test_lower_sellin():
+def test_lower_sellin_with_multiple_items():
     sellin = 1
     sellin_2 = -1
     items = [Item("item", sellin, 1), Item("item", sellin_2, 10)]
-
     create_and_update(items)
     assert items[0].sell_in == sellin - 1
     assert items[1].sell_in == sellin_2 - 1
-
-
-def get_new_quality_with_name(name, old_quality, sellin):
-    items = [Item(name, sellin, old_quality)]
-    create_and_update(items)
-    return items[0].quality
-
-
-def get_new_quality(old_quality, sellin):
-    return get_new_quality_with_name("item", old_quality, sellin)
 
 
 class TestDegradeTwiceAsFast:
@@ -71,3 +70,9 @@ def test_brie_increases_value():
 def test_brie_never_beyond_50():
     quality = 50
     assert get_new_quality_with_name("Aged Brie", quality, 2) == quality
+
+
+def test_sulfuras_never_decreases():
+    quality = 80
+    assert get_new_quality_with_name("Sulfuras, Hand of Ragnaros", quality, 2) == quality
+
