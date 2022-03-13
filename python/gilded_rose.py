1536b051c066157c93942b902a94c0e2618e106f
Adrian Stobbe <stobbe.adrian@gmail.com>
2022-03-13 17:39:29 +0100

    added AgedBrie class

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -16,6 +16,20 @@ class Sulfuras:
     def update_quality(self):
         return
 
+
+class AgedBrie:
+    def __init__(self, item: Item):
+        self.item = item
+
+    def update_quality(self):
+        self.item.sell_in -= 1
+        quality_inc = 1
+        if self.item.sell_in <= 0:
+            quality_inc = 2
+        self.item.quality += quality_inc
+        self.item.quality = min(self.item.quality, 50)
+
+
 class OtherItems:
     def __init__(self, item: Item):
         self.item = item
@@ -23,8 +37,7 @@ class OtherItems:
     def update_quality(self):
         if self.item.name != "Aged Brie" and self.item.name != "Backstage passes to a TAFKAL80ETC concert":
             if self.item.quality > 0:
-                if self.item.name != "Sulfuras, Hand of Ragnaros":
-                    self.item.quality = self.item.quality - 1
+                self.item.quality = self.item.quality - 1
         else:
             if self.item.quality < 50:
                 self.item.quality = self.item.quality + 1
@@ -35,14 +48,12 @@ class OtherItems:
                     if self.item.sell_in < 6:
                         if self.item.quality < 50:
                             self.item.quality = self.item.quality + 1
-        if self.item.name != "Sulfuras, Hand of Ragnaros":
-            self.item.sell_in = self.item.sell_in - 1
+        self.item.sell_in = self.item.sell_in - 1
         if self.item.sell_in < 0:
             if self.item.name != "Aged Brie":
                 if self.item.name != "Backstage passes to a TAFKAL80ETC concert":
                     if self.item.quality > 0:
-                        if self.item.name != "Sulfuras, Hand of Ragnaros":
-                            self.item.quality = self.item.quality - 1
+                        self.item.quality = self.item.quality - 1
                 else:
                     self.item.quality = self.item.quality - self.item.quality
             else:
@@ -53,9 +64,12 @@ class OtherItems:
 def create_smart_item(item: Item):
     if item.name == "Sulfuras, Hand of Ragnaros":
         return Sulfuras(item)
+    elif item.name == "Aged Brie":
+        return AgedBrie(item)
     else:
         return OtherItems(item)
-        
+
+
 class GildedRose(object):
     def __init__(self, items):
         self.items = items
@@ -63,6 +77,7 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             create_smart_item(item).update_quality()
+
 
 class Item:
     def __init__(self, name, sell_in, quality):
