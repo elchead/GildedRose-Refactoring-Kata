33830a7775036ae1b03a2bfbd410ca48f4968640
Adrian Stobbe <stobbe.adrian@gmail.com>
2022-03-13 16:16:23 +0100

    test parameterized

--- python/test_glided.py
+++ python/test_glided.py
@@ -2,9 +2,28 @@ import pytest
 from gilded_rose import *
 
 
-def test_zero_never_negative():
-    items = [Item("neg", 0, 0)]
-
+def create_and_update(items):
     sut = GildedRose(items)
     sut.update_quality()
-    assert sut.items[0].quality == 0
+
+
+@pytest.mark.parametrize("quality", [0, 1, 2, 3, 50])
+def test_never_negative(quality):
+    items = [Item("item", 0, quality)]
+
+    create_and_update(items)
+    assert items[0].quality >= 0
+
+
+def test_lower_quality():
+    quality = 1
+    quality_2 = 2
+    items = [Item("item", 10, quality), Item("item", 10, quality_2)]
+
+    create_and_update(items)
+    assert items[0].quality == quality - 1
+    assert items[1].quality == quality_2 - 1
+
+
+def test_lower_sellin():
+    pass
